032d8ee4d7b17ec34c6af49a161e88d4fd138bd1
mynguyenduc9x <mynguyenduc9x@gmail.com>
2023-06-26 22:20:02 +0700

    create admin fe repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import {AppRouting} from "./app-routing";
+import {RouterOutlet} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting,
+    RouterOutlet,
+    FormsModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/authen-response.ts
@@ -0,0 +1,35 @@
+export class AuthenResponse {
+  private _code: string;
+  private _token: any;
+  private _status: string | undefined
+
+  constructor(private res: any) {
+    this._code = res.code;
+    this._token = res.token.toString();
+    this._status = res.status;
+  }
+
+  get code(): string {
+    return this._code;
+  }
+
+  set code(value: string) {
+    this._code = value;
+  }
+
+  get token(): any {
+    return this._token;
+  }
+
+  set token(value: any) {
+    this._token = value;
+  }
+
+  get status(): string | undefined {
+    return this._status;
+  }
+
+  set status(value: string | undefined) {
+    this._status = value;
+  }
+}
